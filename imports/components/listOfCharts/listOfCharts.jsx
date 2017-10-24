59ff2bfde3343d7053d812f5000c56f58fcdd4a7
mikze <mikzepro@gmail.com>
2017-10-25 00:20:52 +0200

    some fixes

--- imports/components/listOfCharts/listOfCharts.jsx
+++ imports/components/listOfCharts/listOfCharts.jsx
@@ -13,12 +13,15 @@ export default class ListOfCharts extends React.Component{
         this.state = {
             data: props.data,
             open: false,
+            create:false,
             params: ['xD'],
             lines: [],
           };
     
                 this.handleOpen = this.handleOpen.bind(this);
                 this.handleClose = this.handleClose.bind(this);
+                this.handleCreate = this.handleCreate.bind(this);
+                this.handleCreateClose = this.handleCreateClose.bind(this);
       }
 
       componentWillReceiveProps(nextProps, nextState) {
@@ -34,27 +37,60 @@ handleOpen = lines => {
     this.setState({open: false});
   };
 
+  handleCreate = () => {
+    this.setState({create: true});
+  };
+
+  handleCreateClose = () => {
+    this.setState({create: false});
+  };
+
 render()
 {
+
     const actions = [
         <FlatButton
           label="Cancel"
           primary={true}
           onClick={this.handleClose}
         />,
+        <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleCreateClose}
+      />,
       ];
 
-    return(<div>{this.props.measurements.map(x => <List><ListItem primaryText={x.name} onClick={() => this.handleOpen(x.lines)}/></List>)} 
-    
+    return(<div>
+        
+        <FlatButton
+        label="Create New Chart"
+        primary={true}
+        onClick={(this.handleCreate)}
+        />
+        
+        {this.props.measurements.map(x => <List><ListItem primaryText={x.name} onClick={() => this.handleOpen(x.lines)}/></List>)} 
+        
+        <Dialog
+        title="Dialog With Actions"
+        actions={actions[1]}
+        modal={false}
+        open={this.state.create}
+        onRequestClose={this.handleCreateClose}
+      >
+      Create new chart
+      </Dialog>
+
     <Dialog
     title="Dialog With Actions"
-    actions={actions}
+    actions={actions[0]}
     modal={false}
     open={this.state.open}
     onRequestClose={this.handleClose}
   >
   <Chart data={this.state.data} params={this.state.params} lines={this.state.lines}/>
   </Dialog>
+    
     </div>)
 }
 }
--- imports/components/measurement/measurement.jsx
+++ imports/components/measurement/measurement.jsx
@@ -2,24 +2,41 @@ import React from 'react'
 import FlatButton from 'material-ui/FlatButton';
 import ShowData from '../showData'
 import ListOfCharts from '../listOfCharts'
-
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 
 export default class Measurement extends React.Component {
 
     constructor(props) {
     super(props);
-    this.state = {data: props.measurements.data};
+    this.state = {data: props.measurements.data,
+                  close: false};
+
+    this.handleOpen = this.handleOpen.bind(this);
   }
 
   componentWillReceiveProps(nextProps, nextState) {
     this.setState({data:nextProps.measurements.data});
   }
 
+  handleOpen = () => {
+    this.state.close ? this.setState({close: false}) : this.setState({close: true});
+  };
+
+
   render()
     {
     return(<div>{this.props.match.params.id}<br/>
-           <ListOfCharts id={this.props.match.params.id} data={this.state.data}/>
+      <FlatButton
+      label="Show/Hide Charts List"
+      primary={true}
+      onClick={(this.handleOpen)}
+      />   
+           <br/>
+           {this.state.close ? <Card><CardHeader
+            title="List of measurements"
+            />
+            <ListOfCharts id={this.props.match.params.id} data={this.state.data}/></Card> : null}
            </div>)
     }
 }
